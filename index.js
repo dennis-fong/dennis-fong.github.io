7f731ff6eadd74b9ae30a31586a2b738bdf85190
dennis-fong <104570392+dennis-fong@users.noreply.github.com>
2023-10-31 11:23:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-
 // 連接數據庫
 const connection = new MySQLConnection({
     host: 'rm-bp1b191062h53s0n0po.mysql.rds.aliyuncs.com',
     user: 'dennis',
     password: 'aA28450764#',
     database: 'testdb',
-    port:'3306',
+    post:'3306'
   });
   
-  // 查詢數據
-  connection.query('SELECT * FROM test1', (err, results) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  
-    // 顯示數據
-    const table = document.querySelector('table');
-    results.forEach((row) => {
-      const tr = document.createElement('tr');
-      tr.innerHTML = `
-        <td>${row.name}</td>
-      `;
-      table.appendChild(tr);
-    });
-  });
-  
-  // 關閉連接
-  connection.end();
+// 連接成功後執行的回調函數
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    // 連接成功
+    console.log("連接成功");
+
+  }
+});
